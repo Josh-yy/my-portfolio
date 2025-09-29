63cfa374c7f5a4d3bc8d6c621911daf2373dc16c
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-09-29 16:24:43 +0800

    fix: remove notice modal; adjust loading animation across device viewport

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Outlet, ScrollRestoration } from "react-router-dom";
 import CursorEffect from "./components/CursorEffect/CursorEffect.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import Loading from "./components/Loading/Loading.jsx";
-import Modal from "./components/Modal/Modal.jsx";
 import "./index.css";
 
 function App() {
@@ -10,12 +9,7 @@ function App() {
     <>
       <CursorEffect />
       <Loading />
-      <Modal />
-
       <Outlet />
-
-      
-
       <Footer />
       <ScrollRestoration />
     </>
